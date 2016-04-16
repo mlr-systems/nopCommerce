17912cbafba903fd5bf0c5a99977b6926e6786cb
Jason Krause <jason@pbcdev.com>
2016-04-16 16:50:56 -0700

    Changed Namespace to Nop.Plugin.Misc.MLR.Admin.   Plugin installs and creates the BusinessCustomer table in the database

---
+++ Plugins/Nop.Plugin.Widgets.MLR.Admin/Data/BusinessCustomerMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Nop.Plugin.Misc.MLR.Admin.Domain;
+
+namespace Nop.Plugin.Misc.MLR.Admin.Data
+{
+    public class BusinessCustomerMap : EntityTypeConfiguration<BusinessCustomer>
+    {
+        public BusinessCustomerMap()
+        {
+            ToTable("BusinessCustomer");
+
+            HasKey(x => x.BusinessCustomerId);
+            Property(x => x.BusinessName);
+            Property(x => x.BusinessCustomerContact);
+            Property(x => x.ResellerNumber);
+            Property(x => x.W9);
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Widgets.MLR.Admin/MlrAdminPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Routing;
+using Nop.Core.Plugins;
+using Nop.Plugin.Misc.MLR.Admin.Data;
+using Nop.Services.Common;
+
+namespace Nop.Plugin.Misc.MLR.Admin
+{
+    public class MlrAdminPlugin : BasePlugin, IMiscPlugin
+    {
+        private MlrAdminContext _context;
+
+        public MlrAdminPlugin(MlrAdminContext context)
+        {
+            _context = context;
+        }
+
+        public override void Install()
+        {
+            _context.Install();
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            _context.Uninstall();
+            base.Uninstall();
+        }
+
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "MlrAdmin";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Misc.MLR.Admin.Controllers" }, { "area", null } };
+        }
+    }
+}
