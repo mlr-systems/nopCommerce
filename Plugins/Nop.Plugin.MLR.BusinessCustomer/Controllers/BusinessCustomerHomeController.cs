a10865c0f05d7cd23a84baa230f0d5f54e49057a
Jason Krause <jason@pbcdev.com>
2016-04-28 10:10:24 -0700

    Paging works on the grid with just a few records all the way up to 100 (probably more but all I had in test), added the edit route and created the file that the edit and create methods will share.

--- Plugins/Nop.Plugin.MLR.BusinessCustomer/Controllers/BusinessCustomerHomeController.cs
+++ Plugins/Nop.Plugin.MLR.BusinessCustomer/Controllers/BusinessCustomerHomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
+using Nop.Core;
 using Nop.Core.Data;
 using Nop.Services.Security;
 using Nop.Web.Framework.Controllers;
@@ -22,11 +23,14 @@ namespace Nop.Plugin.MLR.BusinessCustomer.Controllers
         public ActionResult BusinessCustomerList(DataSourceRequest command, Domain.BusinessCustomer model)
         {
 
-            var businessCustomers = _businessCustomerRepo.Table;
+            var businessCustomers = _businessCustomerRepo.Table.OrderBy(x => x.BusinessCustomerId);
+
+            IPagedList<Domain.BusinessCustomer> pagedBusinessCustomers = 
+                    new PagedList<Domain.BusinessCustomer>(businessCustomers, command.Page - 1, command.PageSize);
 
             var gridModel = new DataSourceResult
             {
-                Data = businessCustomers,
+                Data = pagedBusinessCustomers,
                 Total = businessCustomers.Count()
             };
 
@@ -40,16 +44,24 @@ namespace Nop.Plugin.MLR.BusinessCustomer.Controllers
 
         public ActionResult List()
         {
-            //var businessCustomers = _businessCustomerRepo.Table.ToList();
-
             return View("~/Plugins/MLR.BusinessCustomer/Views/BusinessCustomerHome/ListBusinessCustomers.cshtml");
         }
 
-        //[ChildActionOnly]
-        //[AdminAuthorize]
         public ActionResult Configure()
         {
             return View("~/Plugins/MLR.BusinessCustomer/Views/BusinessCustomerHome/Configure.cshtml");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var businessCustomer = _businessCustomerRepo.Table.Single(x => x.Id == id);
+
+            if (businessCustomer == null)
+            {
+                RedirectToAction("List", "BusinessCustomerHome");
+            }
+
+            return View("~/Plugins/MLR.BusinessCustomer/Views/BusinessCustomerHome/Edit.cshtml", businessCustomer);
         }
     }
 }
